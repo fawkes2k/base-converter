20506041d0bc040edb795df70acab558fffb09c1
Dominik Foks <93789489+fawkes2k@users.noreply.github.com>
2023-01-18 02:26:59 +0100

    Update base_converter.py

--- base_converter.py
+++ base_converter.py
@@ -1,3 +1,5 @@
+class IncorrectArgumentException(RuntimeError): pass
+
 class BaseConverter:
     def __init__(self, base: int, base_digits: str):
         """
@@ -8,9 +10,9 @@ class BaseConverter:
         :return: A conversion object
         """
         if base <= 1 or base % 1 != 0:
-            raise Exception(f'{base} is not a natural number or/and not greater than 1')
+            raise IncorrectArgumentException(f'{base} is not a natural number or/and not greater than 1')
         if len(base_digits) != base:
-            raise Exception(f'The number of digits ({len(base_digits)}) and base ({base}) do not match')
+            raise IncorrectArgumentException(f'The number of digits ({len(base_digits)}) and base ({base}) do not match')
         self.base, self.digits = base, base_digits
 
     def decimal_to_base(self, number: int) -> str:
@@ -21,7 +23,7 @@ class BaseConverter:
         :return: A number in given positional numeral system
         """
         from math import log
-        if number < 0 or number % 1 != 0: raise Exception(f'{number} is not a natural number')
+        if number < 0 or number % 1 != 0: raise IncorrectArgumentException(f'{number} is not a natural number')
         if number == 0: return self.digits[number]
         length, final = int(log(number) / log(self.base) + 1), ''
         for power in range(length):
@@ -37,7 +39,7 @@ class BaseConverter:
         :return: A number in decimal
         """
         for digit in number:
-            if digit not in self.digits: raise Exception(f"Digit '{digit}' is not used by this numberal system")
+            if digit not in self.digits: raise IncorrectArgumentException(f"Digit '{digit}' is not used by this numberal system")
         length, decimal = len(number), 0
         for index in range(length): decimal += self.digits.find(number[length - index - 1]) * self.base**index
         return decimal
